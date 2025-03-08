f23bdba5c611bda7fb66d892d112a518a029dd9c
striverz <striverzarc@gmail.com>
2025-03-08 09:00:27 +0530

    implemented the dynamic request review page

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,11 +2,20 @@ import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../redux/requestsSlics";
+import { addRequests, removeRequest } from "../redux/requestsSlics";
 
 const Requests = () => {
   const dispatch = useDispatch();
   const requests = useSelector((store) => store.requests);
+
+  const hadleRequestStatus = async (status, id) => {
+    const review = await axios.post(
+      BASE_URL + "/request/review/" + status + "/" + id,
+      {},
+      { withCredentials: true }
+    );
+    dispatch(removeRequest(id));
+  };
   const fetchRequests = async () => {
     try {
       const requests = await axios.get(BASE_URL + "/user/requests", {
@@ -28,13 +37,26 @@ const Requests = () => {
   return (
     <div>
       {requests &&
-        requests.map((request) => {
+        requests.map((request, ind) => {
           return (
             request.fromUserId && (
-              <div className="flex items-center justify-center m-4 p-4">
+              <div
+                className="flex items-center justify-center m-4 p-4"
+                key={ind}
+              >
                 <h1 className="p-5">{request?.fromUserId?.firstName}</h1>
-                <button className="btn btn-primary m-4">Reject</button>
-                <button className="btn btn-secondary">Accept</button>
+                <button
+                  className="btn btn-primary m-4"
+                  onClick={() => hadleRequestStatus("rejected", request._id)}
+                >
+                  Reject
+                </button>
+                <button
+                  className="btn btn-secondary"
+                  onClick={() => hadleRequestStatus("accepted", request._id)}
+                >
+                  Accept
+                </button>
               </div>
             )
           );
