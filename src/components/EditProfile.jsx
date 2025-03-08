cfcfc5142087340c4535c65ea42e30e281cf34f6
striverz <striverzarc@gmail.com>
2025-03-08 06:26:20 +0530

    created the dynamic profile edit section page

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 import FeedCard from "./FeedCard";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
 
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
-  const [photoUrl, setPhotoUrl] = useState(user.photoURL);
+  const [photoURL, setPhotoURL] = useState(user.photoURL);
   const [about, setAbout] = useState(user.about);
+
+  const handleUpdateProfile = async () => {
+    const profile = await axios.patch(
+      BASE_URL + "/profile/edit",
+      {
+        firstName,
+        lastName,
+        about,
+        photoURL,
+      },
+      { withCredentials: true }
+    );
+  };
 
   return (
     <div className="flex flex-col md:flex-row justify-center items-stretch gap-10 p-5">
@@ -46,8 +61,8 @@ const EditProfile = ({ user }) => {
               <input
                 type="text"
                 className="input input-bordered w-full"
-                value={photoUrl}
-                onChange={(e) => setPhotoUrl(e.target.value)}
+                value={photoURL}
+                onChange={(e) => setPhotoURL(e.target.value)}
               />
             </label>
 
@@ -65,7 +80,12 @@ const EditProfile = ({ user }) => {
           </div>
 
           <div className="card-actions justify-center mt-4">
-            <button className="btn btn-primary w-full">Save</button>
+            <button
+              className="btn btn-primary w-full"
+              onClick={handleUpdateProfile}
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>
@@ -74,7 +94,7 @@ const EditProfile = ({ user }) => {
       <div className="w-full max-w-md flex-1">
         <div className="h-full flex">
           <FeedCard
-            users={{ firstName, lastName, about, photoUrl }}
+            users={{ firstName, lastName, about, photoURL }}
             className="flex-1"
           />
         </div>
